186f94da7d255f0db4cee5687a33eeafc6f4718e
ildar <ildar.kashafetdinov@gmail.com>
2023-05-13 19:13:13 +0300

    class "BankAccount"

---
+++ task_14.py
@@ -0,0 +1,10 @@
+class Student:
+
+    def __init__(self, name, surname, age, profession):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.profession = profession
+
+    def student_info(self):
+        print(f"{self.name} {self.surname}, {self.age}, {self.profession}")
