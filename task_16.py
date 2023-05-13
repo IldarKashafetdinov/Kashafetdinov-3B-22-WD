5922632985fee83064e7beea6e9a393e4450ea45
ildar <ildar.kashafetdinov@gmail.com>
2023-05-13 19:36:52 +0300

    class "Cat"

---
+++ task_16.py
@@ -0,0 +1,9 @@
+class Cat:
+
+    def __init__(self, name, age, color):
+        self.name = name
+        self.age = age
+        self.color = color
+
+    def cat_info(self):
+        print(f"Кошка по имени {self.name}, возраст {self.age}, цвет {self.color}")
