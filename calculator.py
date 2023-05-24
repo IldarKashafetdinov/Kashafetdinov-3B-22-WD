a7656b5cb7fe3b0803d4f3ed9a81d89c743f74a7
ildar <ildar.kashafetdinov@gmail.com>
2023-05-24 14:25:14 +0300

    h/w3 done

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def get_summ(a, b):
+    return (f"{a} + {b} = {a + b}")
+
+def get_substraction(a, b):
+    return (f"{a} - {b} = {a - b}")
+
+def get_multiplication(a, b):
+    return (f"{a} * {b} = {a * b}")
+
+def get_division(a, b):
+    return (f"{a} / {b} = {a / b}")
